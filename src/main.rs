99b3fa140fe9acb4443906349dc992e8d0bbd13d
Jeffrey Yu <92849+wehjin@users.noreply.github.com>
2023-08-05 11:07:51 -0700

    Draws happy face in canvas (from wasm-bindgen examples).

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,51 @@
+extern crate web_sys;
+
 use wasm_bindgen::prelude::*;
 
-fn main() {
+//noinspection RsUnresolvedReference
+fn main() -> Result<(), JsValue> {
 	wasm_logger::init(wasm_logger::Config::default());
 	log::info!("Boo!");
+	let document = web_sys::window().unwrap().document().unwrap();
+	let body = document.body().expect("document should have a body");
+	let canvas = document.create_element("canvas")?;
+	let canvas: web_sys::HtmlCanvasElement = canvas
+		.dyn_into::<web_sys::HtmlCanvasElement>()
+		.map_err(|_| ())
+		.unwrap();
+	let context = canvas
+		.get_context("2d")
+		.unwrap()
+		.unwrap()
+		.dyn_into::<web_sys::CanvasRenderingContext2d>()
+		.unwrap();
+
+	context.begin_path();
+
+	// Draw the outer circle.
+	context
+		.arc(75.0, 75.0, 50.0, 0.0, std::f64::consts::PI * 2.0)
+		.unwrap();
+
+	// Draw the mouth.
+	context.move_to(110.0, 75.0);
+	context.arc(75.0, 75.0, 35.0, 0.0, std::f64::consts::PI).unwrap();
+
+	// Draw the left eye.
+	context.move_to(65.0, 65.0);
+	context
+		.arc(60.0, 65.0, 5.0, 0.0, std::f64::consts::PI * 2.0)
+		.unwrap();
+
+	// Draw the right eye.
+	context.move_to(95.0, 65.0);
+	context
+		.arc(90.0, 65.0, 5.0, 0.0, std::f64::consts::PI * 2.0)
+		.unwrap();
+
+	context.stroke();
+	body.append_child(&canvas)?;
+	Ok(())
 }
 
 #[wasm_bindgen]
