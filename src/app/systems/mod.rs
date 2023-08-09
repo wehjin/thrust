96a6712179ca9d61ae552acee7e916b5006afcf6
Jeffrey Yu <92849+wehjin@users.noreply.github.com>
2023-08-09 11:47:26 -0700

    Refactors init_world and add_cube in main.

--- src/app/systems/mod.rs
+++ src/app/systems/mod.rs
@@ -1,3 +1,2 @@
 pub mod spin_system;
-pub mod build_cube;
 
--- src/main.rs
+++ src/main.rs
@@ -11,28 +11,17 @@ use crate::app::components::spin_target::SpinTarget;
 use crate::app::systems::spin_system::SpinSystem;
 use crate::dom::{Error};
 use crate::four::render_system::RenderSystem;
+use crate::three::Scene;
 
 fn main() -> Result<(), Error> {
 	wasm_logger::init(wasm_logger::Config::default());
 	log::info!("Boo!");
-
-	let mesh = {
-		let geometry = three::BoxGeometry::new(2., 1., 1.);
-		let material = three::MeshBasicMaterial::new();
-		material.color().set(0., 0., 255.);
-		three::Mesh::new(geometry, material)
-	};
-	mesh.position().set_xyz(0., 1.6, -3.);
-	mesh.rotation().set(0., 0., 0.);
-
 	let renderer = four::Renderer::new(&Window::connect());
-	renderer.scene().add(&mesh);
-
 	let mut world = World::new();
 	world.register::<SpinTarget>();
 	world.insert(NowInSeconds(0.));
 	world.insert(renderer.clone());
-	world.create_entity().with(SpinTarget { euler: mesh.rotation() }).build();
+	init_world(&mut world, renderer.scene());
 
 	let mut dispatcher = DispatcherBuilder::new()
 		.with_thread_local(SpinSystem)
@@ -45,4 +34,21 @@ fn main() -> Result<(), Error> {
 		world.maintain();
 	});
 	Ok(())
+}
+
+fn init_world(world: &mut World, scene: &three::Scene) {
+	add_cube(world, scene);
+}
+
+fn add_cube(world: &mut World, scene: &Scene) {
+	let mesh = {
+		let geometry = three::BoxGeometry::new(2., 1., 1.);
+		let material = three::MeshBasicMaterial::new();
+		material.color().set(0., 0., 255.);
+		three::Mesh::new(geometry, material)
+	};
+	mesh.position().set_xyz(0., 1.6, -3.);
+	mesh.rotation().set(0., 0., 0.);
+	scene.add(&mesh);
+	world.create_entity().with(SpinTarget { euler: mesh.rotation() }).build();
 }
