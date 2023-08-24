77e75435fac848002d08f1943a6799f0c22520a8
Jeffrey Yu <92849+wehjin@users.noreply.github.com>
2023-08-24 12:30:32 -0700

    Change to VRButton from node_modules.  Adds viewport and css.

--- src/three/vr_button.rs
+++ src/three/vr_button.rs
@@ -2,7 +2,7 @@ use wasm_bindgen::prelude::*;
 use web_sys::HtmlButtonElement;
 use crate::three::WebGLRenderer;
 
-#[wasm_bindgen(module = "/vendor/three/VRButton.js")]
+#[wasm_bindgen(module = "/node_modules/three/examples/jsm/webxr/VRButton.js")]
 extern "C" {
 	#[wasm_bindgen(js_namespace = VRButton, js_name = createButton)]
 	pub fn create_button(renderer: &WebGLRenderer) -> HtmlButtonElement;
